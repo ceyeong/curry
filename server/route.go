89ffcd61767bd43c9ce63a3b1f350a13f620badf
gurung <maandipatmmu@gmail.com>
2020-04-06 23:15:47 +0900

    add name to new routes

--- server/route.go
+++ server/route.go
@@ -10,8 +10,8 @@ func route(e *echo.Echo) {
 	apiV1 := e.Group("/api/v1")
 	apiV1.POST("/register", handler.RegisterUser).Name = "register"
 	apiV1.POST("/login", handler.LoginUser).Name = "login"
-	apiV1.POST("/logout", handler.LogoutUser)
+	apiV1.POST("/logout", handler.LogoutUser).Name = "logout"
 
 	apiV1.GET("/me", handler.Me).Name = "me"
-	apiV1.GET("/csrf", handler.Csrf)
+	apiV1.GET("/csrf", handler.Csrf).Name = "csrf"
 }
